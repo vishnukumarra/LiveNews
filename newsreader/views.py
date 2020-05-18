257c197a2959617245b6b25d2c3337b55f97692c
vasanthmaxwell <vasanthmaxwell325@gmail.com>
2020-05-18 01:22:27 -0700

    firstcommit

---
+++ newsreader/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from newsapi import NewsApiClient
+
+
+
+def Index(request):
+    newsapi = NewsApiClient(api_key="3261097a702947809aad72c0ab3c2291")
+    topheadlines = newsapi.get_top_headlines(country="in",category="technology")
+
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+
+    mylist = zip(news, desc, img)
+
+
+    return render(request, 'mainpage.html', context={"mylist":mylist})
+
+
+
+def bbc(request):
+    newsapi = NewsApiClient(api_key="3261097a702947809aad72c0ab3c2291")
+    topheadlines = newsapi.get_top_headlines(sources='bbc-news')
+
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+
+    mylist = zip(news, desc, img)
+
+
+    return render(request, 'newspage.html', context={"mylist":mylist})
